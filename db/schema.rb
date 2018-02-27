97ca3acdf6b1d78b1cedbdc462ce5da0474ae381
Konstantinos Mitsainas <mitsenask@gmail.com>
2018-02-27 12:54:59 +0100

    add user references to activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227112330) do
+ActiveRecord::Schema.define(version: 20180227115406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20180227112330) do
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
     t.string "sku"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
   create_table "bookings", force: :cascade do |t|
@@ -94,6 +96,7 @@ ActiveRecord::Schema.define(version: 20180227112330) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
   add_foreign_key "bookings", "events"
   add_foreign_key "bookings", "users"
   add_foreign_key "events", "activities"
