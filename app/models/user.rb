1fae99445c83a6b68342b613340a2acf83741e2e
Konstantinos Mitsainas <mitsenask@gmail.com>
2018-02-27 13:10:36 +0100

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  has_many :activities
   has_many :bookings
   has_many :events, through: :bookings
   has_many :reviews, through: :bookings
-  has_many :activities, through: :events
+  # has_many :somethings, through: :events, foreign_key: "activity_id", class_name: "Activity"
+  has_many :plans, :through => :events, :source => :activity
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
