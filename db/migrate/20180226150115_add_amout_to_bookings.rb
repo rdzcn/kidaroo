23bea881165296f8835bd579bb0ad72f02d574c8
Arda Ozcan <ardaoezcan@gmail.com>
2018-03-02 14:41:07 +0100

    schema

--- db/migrate/20180226150115_add_amout_to_bookings.rb
+++ db/migrate/20180226150115_add_amout_to_bookings.rb
@@ -1,6 +1,6 @@
 class AddAmoutToBookings < ActiveRecord::Migration[5.1]
   def change
-    add_monetize :bookings, :amount, currency: { present: false }
+    add_monetize :bookings, :amount_cents, currency: { present: false }
     add_column :bookings, :state, :string
     add_column :bookings, :activities_sku, :string
     add_column :bookings, :payment, :jsonb
