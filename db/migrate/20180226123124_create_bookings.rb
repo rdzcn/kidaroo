d6a870ad33770bc140f66c99c1c5fcc8aceee7f0
AnnaMaria Wojtyga <annamaria.wojtyga@gmail.com>
2018-03-02 12:16:54 +0100

    styling

--- db/migrate/20180226123124_create_bookings.rb
+++ db/migrate/20180226123124_create_bookings.rb
@@ -3,6 +3,7 @@ class CreateBookings < ActiveRecord::Migration[5.1]
     create_table :bookings do |t|
       t.references :user, foreign_key: true
       t.references :activity
+      t.monetize :amount, currency: { present: false }
       t.timestamps
     end
   end
