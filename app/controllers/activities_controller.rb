46b8db137d73fd307d2086d31b6fe7bd5eec6d44
Konstantinos Mitsainas <mitsenask@gmail.com>
2018-03-04 16:41:55 +0100

    filters working

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,14 +2,21 @@ class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit]
 
   def index
+    district = []
+    cat = []
+    age = []
+    Activity.all.each do |activity|
+      district << activity.address
+      cat << activity.category
+      age << activity.age_group
+    end
     @activities = Activity.all
-    @activities = @activities.where(category: params[:category]) unless params[:category].blank?
-    @activities = @activities.where(age_group: params[:age_group]) unless params[:age_group].blank?
+    @activities = @activities.where("category ILIKE ?", params[:category]) unless (params[:category].blank? || !cat.include?(params[:category]))
+    @activities = @activities.where(age_group: params[:age_group]) unless (params[:age_group].blank? || !age.include?(params[:age_group]))
     titles = params[:title].split(' ') unless params[:title].blank?
     @activities = @activities.where((["title ILIKE ?"] * titles.size).join(' OR '), *titles.map { |title| "%#{title}%" }) unless titles.blank?
-    addr = params[:address].split(' ') unless params[:address].blank?
+    addr = params[:address].split(' ') unless (params[:address].blank? || !district.include?(params[:address]))
     @activities = @activities.where((["address ILIKE ?"] * addr.size).join(' OR '), *addr.map { |address| "%#{address}%" }) unless addr.blank?
-
     @activities = @activities.where.not(latitude: nil, longitude: nil) unless params[:address].blank?
     @markers = @activities.map do |activity|
       next if activity.latitude.nil?
