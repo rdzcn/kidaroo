a12178853cf8e2b3e3ae0df3fef1efdbf3246ab4
AnnaMaria Wojtyga <annamaria.wojtyga@gmail.com>
2018-03-05 12:22:54 +0100

    great

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 	skip_before_action :configure_permitted_parameters, if: :devise_controller?, only: [:home]
 
 	def home
-		@activites = Activity.all 
+		@activities = Activity.all 
+		@events = Event.all
 	end
 end
