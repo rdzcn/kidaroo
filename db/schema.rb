a64e973a9ad4079baae89be5abca2b9bfff5ed5b
Konstantinos Mitsainas <mitsenask@gmail.com>
2018-03-05 10:27:47 +0100

    add district to activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228120033) do
+ActiveRecord::Schema.define(version: 20180305091215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180228120033) do
     t.bigint "user_id"
     t.float "latitude"
     t.float "longitude"
+    t.string "district"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
