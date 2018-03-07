2a8d4cd9c019e2a244ca4ad74d13ea2e71c0cc1e
AnnaMaria Wojtyga <annamaria.wojtyga@gmail.com>
2018-03-07 13:43:36 +0100

    fucking grids - I will learn you after bootcamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305202529) do
+ActiveRecord::Schema.define(version: 20180307110704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20180305202529) do
     t.string "address"
     t.string "photos"
     t.string "category"
-    t.integer "age_group"
+    t.integer "age_group", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
